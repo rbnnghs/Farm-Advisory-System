99399dca7d929e69158f7d9380dabfde1152d56c
Robin Naghshbandi <robinnaghshbandi12@gmail.com>
2022-05-06 06:38:30 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import LevelInfo from '../components/LevelInfo'
 import Head from 'next/head';
 
 export default function App () {
-  <Head>
-  <link rel='FAS' href='/fas.ico'/>
-  </Head>
+  // <Head>
+  // <link rel='FAS' href='/fas.ico'/>
+  // </Head>
   return <LevelInfo id={1} />
 }
